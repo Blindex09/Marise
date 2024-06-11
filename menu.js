b1b1f30e7f86b397310de3f47187e2f44cfe350c
Blindex09 <149652979+Blindex09@users.noreply.github.com>
2024-06-11 18:42:59 -0300

    Adicionado um link para voltar ao menu de navegação. O script menu.js está refenciado no index.htm

---
+++ menu.js
@@ -0,0 +1,11 @@
+    function focusFirstMenuItem() {
+        document.getElementById('sobre-mim-li
+    function focusFirstMenuItem() {
+        document.getElementById('sobre-mim
+    function focusFirstMenuItem() {
+        document.getElementById('sobre-m
+    function focusFirstMenuItem() {
+        document.getElementById('sobre
+    function focusFirstMenuItem() {
+        document.getElementById('sobre-mim-link').focus();
+    }

534ca55695b53c341b94ed721ff716ef088b1999
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2024-07-07 10:25:05 -0300

    fix: adiciona novas fotos ao carrossel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
-  $(".carrossel").slick({
-    dots: true,
-    arrows: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-  });
+  $(".carrossel")
+    .slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+    })
+    .on("afterChange", function () {
+      $(".slick-current")
+        .addClass("d-flex")
+        .addClass("align-items-center")
+        .addClass("justify-content-center");
+    });
 });
